3af80f83318ca68bf37ec1b4ee242a6262a51e2c
squireren <squireren@gmail.com>
2026-08-27 20:26:22 -0500

    [0.4.0] chore: bump firmware version to 0.4.0
    - version.py: Increment FIRMWARE_VERSION from 0.3.0 to 0.4.0

--- version.py
+++ version.py
@@ -2,6 +2,6 @@
 # Copyright (c) 2026 dodson Software ( dodson labs )
 # SPDX-License-Identifier: MIT
 
-FIRMWARE_VERSION = "0.3.0"
+FIRMWARE_VERSION = "0.4.0"
 CONFIG_SCHEMA_VERSION = 4
 MESSAGE_SCHEMA_VERSION = 3
